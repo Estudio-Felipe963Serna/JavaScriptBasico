befd183fbde24c64f9390cb9ba3ca805084addd0
Felipe Serna <juan.serna@asys.edu.co>
2024-08-29 20:42:35 -0500

    cambios en clase

---
+++ SemanaVI/comentarios/index.js
@@ -0,0 +1,5 @@
+// esto es un comentario de una sola linea
+/* Esto es un comentario
+en bloque d emultiples
+lineas
+
---
+++ semanaV/1. Variables y constantes/index.js
@@ -0,0 +1,21 @@
+console.log ("Hola mundo");
+console.table ({"Hola": "mundo"});
+console.table ("Hola mundo");
+
+const prueba = "Hola mundo prueba"
+console.log (prueba)
+
+const marcador = "verde";
+const marcador2 = "rojo";
+const marcador3 = "marcador negro"
+const marcadores = ["rojo","verde","azul","negro"];
+
+var variableVar = "Esta es una variable var"
+let variableLet = "Esta e suna variable let"
+const variableConst = "Esta e suna variable const"
+
+console.log(variableVar)
+console.log(variableLet);
+console.log(variableConst);
+
+
